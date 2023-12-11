2a5b446f6f7821345f115571a66927f338d20b90
Vitaliy-Kuzmenko <bitlers@yandex.ru>
2023-12-11 11:55:40 +0300

    добавил проверку остатка вопросов, удаление правильного вопроса

--- script.js
+++ script.js
@@ -35,26 +35,42 @@ function getRandomInRange(min, max) {
   }
 
 const fnGame = (qs,an) =>{
-    const indexArr = getRandomInRange(0, 2)
+    const currentQs = qs //Временный массив вопросов
+    const currentAn = an //Временный массив ответов
+    const indexArr = getRandomInRange(0, currentQs.length-1) //случайный выбор вопроса из длины массива
     
-    divQuestion.innerHTML = arrQstions[indexArr]
+    divQuestion.innerHTML = currentQs[indexArr] //вывод вопроса
 
     btnCheckAnswer.onclick = ()=>{
+       
+        //проверка на пустую строку, либо число
         if (inpAnswer.value == ''|| +inpAnswer.value){
             alert("ОШИБОЧКА! Пустая строка или число")
         }else console.log('всё норм')
 
-        const lowerCase = inpAnswer.value.toLowerCase()
-        if (arrAnswers[indexArr] === lowerCase){
+        const lowerCase = inpAnswer.value.toLowerCase() // перевод введённого ответа в нижний регистр
+        
+        //проверка ответа
+        if (currentAn[indexArr] === lowerCase){
             divQuestion.innerHTML = 'ВСЁ ВЕРНО!'
-        } else  divQuestion.innerHTML = `${'ОШИБОЧКА! Вопрос был...'}<p>${arrQstions[indexArr]}</p> `
+            currentAn.splice(indexArr, 1) //удаление ответа если он был правильный
+            currentQs.splice(indexArr,1) // удаление вопроса если ответ правильный
+            console.log(currentQs)
+            console.log(currentAn)
+        } else  divQuestion.innerHTML = `${'ОШИБОЧКА! Вопрос был...'}<p>${currentQs[indexArr]}</p> `
     }
+    //показ правильного ответа
     btnShowAnswer.onclick = ()=>{
-        divQuestion.innerHTML = `${'Эх ты... Всё было очень просто'}<p>${arrAnswers[indexArr]}</p> `
+        divQuestion.innerHTML = `${'Эх ты... Всё было очень просто'}<p>${currentAn[indexArr]}</p> `
+    }
+    if(currentQs.length === 0){
+        console.log('Пусто')
+    } else {
+        console.log("Осталось вопросов" + currentAn.length )
     }
 }
 
 btnStartGame.onclick = () =>{
-    fnGame()
+    fnGame(arrQstions, arrAnswers)
 }
 
